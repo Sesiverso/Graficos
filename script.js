16a76ec12a49634deb66717e3d523803e601b1cf
Sesiverso <94802700+Sesiverso@users.noreply.github.com>
2024-11-07 09:52:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -17,22 +17,8 @@ function generateChart(labels, values, chartType) {
             datasets: [{
                 label: 'Dados do Gráfico',
                 data: values,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
+                backgroundColor: labels.map((_, index) => `rgba(${255 - (index * 30)}, ${99 + (index * 30)}, ${132 + (index * 15)}, 0.2)`),
+                borderColor: labels.map((_, index) => `rgba(${255 - (index * 30)}, ${99 + (index * 30)}, ${132 + (index * 15)}, 1)`),
                 borderWidth: 1
             }]
         },
@@ -43,6 +29,17 @@ function generateChart(labels, values, chartType) {
                     display: true,
                     position: 'top', // Posição da legenda
                     labels: {
+                        generateLabels: function (chart) {
+                            const datasets = chart.data.datasets[0];
+                            return chart.data.labels.map((label, index) => ({
+                                text: label,
+                                fillStyle: datasets.backgroundColor[index],
+                                strokeStyle: datasets.borderColor[index],
+                                lineWidth: 1,
+                                hidden: false,
+                                index: index
+                            }));
+                        },
                         font: {
                             size: 14
                         }
